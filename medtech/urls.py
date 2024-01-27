71997fbecb287fc6d68ccf3756df0f1e88ca429d
Pedro Henrique Venâncio e Clotilde <ph.venancio2@gmail.com>
2024-01-26 22:54:54 -0300

    Criando front-end da página de Login

--- medtech/urls.py
+++ medtech/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('atendentes/', include('atendentes.urls')),
     path('pacientes/', include('pacientes.urls')),
-    path('', include('login.urls')),
+    path('login/', include('login.urls')),
     path('admin/', admin.site.urls),
 ]
