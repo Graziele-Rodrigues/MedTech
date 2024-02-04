a8abaa32d7ca01359e5f73a0074e7a0cec240659
vinicius <vininiquini7@gmail.com>
2024-02-04 15:18:53 -0300

    Criação das paginas .html do CRUD das consultas

--- consultas/urls.py
+++ consultas/urls.py
@@ -3,8 +3,8 @@ from consultas.views import consultas, criar_consulta, editar_consulta, visualiz
 
 urlpatterns = [
     path('consultas/', consultas, name='consultas'),
-    path('criar_consulta/', criar_consulta, name='criar_consulta'),
-    path('editar_consulta/<int:consulta_id>/', editar_consulta, name='editar_consulta'),
-    path('visualizar_consulta/<int:consulta_id>/', visualizar_consulta, name='visualizar_consulta'),
-    path('deletar_consulta/<int:consulta_id>/', deletar_consulta, name='deletar_consulta'),
+    path('criar-consulta/', criar_consulta, name='criar-consulta'),
+    path('editar-consulta/<int:consulta_id>/', editar_consulta, name='editar-consulta'),
+    path('visualizar-consulta/<int:consulta_id>/', visualizar_consulta, name='visualizar-consulta'),
+    path('deletar-consulta/<int:consulta_id>/', deletar_consulta, name='deletar-consulta'),
 ]
