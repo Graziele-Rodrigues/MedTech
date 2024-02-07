c577590056652eec7db6e4dfa23b597d736790b2
Pedro Venâncio <ph.venancio2@gmail.com>
2024-02-07 18:42:59 -0300

    Ajustes Tela Inicial

--- login/views.py
+++ login/views.py
@@ -5,8 +5,5 @@ from django.contrib.auth.forms import AuthenticationForm
 from consultas.models import Consultas
 
 def tela_inicial(request):
-    if request.user.groups.filter(name='profissional_saude').exists():
-        consultas = Consultas.objects.filter(medico=request.user)
-    else:
-        consultas = Consultas.objects.all()
+    consultas = Consultas.objects.all()
     return render(request,'registration/tela-inicial.html', {'consultas': consultas})
